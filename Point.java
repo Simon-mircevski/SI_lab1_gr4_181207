7a59095984372e68f6da9df3c44d4c438678e229
Simon-mircevski <simonmircevski6@gmail.com>
2020-04-10 15:50:09 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -2,7 +2,23 @@ class Point {
         String color
 	String id
 	double x,y;
-
+        Point(double x, double y){
+            this.x=x;
+            this.y=y;
+        }
+        public double getX(){
+            return x;
+        }
+        public double getY(){
+            return y;
+        }
+        public void setX(new_x){
+            this.x=new_x;
+        }
+        public void setY(new_y){
+            this.y=new_y;
+        }
+        
 	//TODO add new variable
 
 	//TODO constructor
@@ -11,10 +27,25 @@ class Point {
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+            bool x_axis=false, y_axis=false;
+            if(xDirection == 'L'){
+                x--;
+            }
+            else if(xDirection == 'R'){
+                x++;
+            }
+            else if(yDirection == 'U'){
+                y--;
+            }
+            else if(yDirection == 'D'){
+                y++;
+            }
+            
 	}
 
 	public void draw () {
 		//TODO
+            return x + " " + y;
 	}
 
 
